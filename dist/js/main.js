52a70fd417518c6d419f81a381468d0e0c682e7e
Mohamed Ahmed <50152939+Mohamed-Ahmed10@users.noreply.github.com>
2022-06-08 13:04:40 +0200

    .gitignore edit

---
+++ dist/js/main.js
@@ -0,0 +1,13 @@
+let showBtn = document.querySelector("#show_btn"),
+    contactsSmall = document.querySelector("#contacts_list");
+
+showBtn.onclick = function () {
+    contactsSmall.classList.toggle("show");
+    this.children[0].classList.toggle("rotate");
+};
+
+// $("body").niceScroll({
+//     cursorcolor: "#D43636",
+//     cursorwidth: "15px",
+//     background: "#CCC",
+// });
